ffee7498f0efbbe68f12bceab3335c6a186d442b
JaehongDev <println@kakao.com>
2023-11-12 01:45:36 +0900

    feat: 주문 항목 생성시 검증 로직 추가

--- src/main/java/christmas/domain/vo/OrderItem.java
+++ src/main/java/christmas/domain/vo/OrderItem.java
@@ -1,6 +1,14 @@
 package christmas.domain.vo;
 
+import christmas.domain.exception.DomainExceptionCode;
+
 public record OrderItem(Food food, int quantity) {
+
+    public OrderItem {
+        if (quantity <= 0) {
+            throw DomainExceptionCode.INVALID_ORDER.createException();
+        }
+    }
 
     public OrderItem(String foodName, int quantity) {
         this(Food.of(foodName), quantity);
