73892890c4fe7f9c5a14954235913fb6627e80fa
JaehongDev <println@kakao.com>
2023-11-12 01:51:22 +0900

    test: 주문 항목 기능 테스트 작성

--- src/test/java/christmas/domain/vo/OrderItemTest.java
+++ src/test/java/christmas/domain/vo/OrderItemTest.java
@@ -1,20 +1,41 @@
 package christmas.domain.vo;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import christmas.domain.exception.DomainExceptionCode;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @DisplayName("[도메인] 주문 항목 테스트")
 class OrderItemTest {
+
+    @Test
+    void 음식의_이름을_가져올_수_있습니다() {
+        Assertions.assertThat(new OrderItem(Food.BBQ_RIBS, 1).getFoodName())
+                .isEqualTo("바비큐립");
+
+    }
+
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+            1,5000
+            2,10000
+            3,15000
+            """)
+    void 음식의_수량에_맞춰_가격을_계산할_수_있습니다(int quantity, int price) {
+        assertThat(new OrderItem(Food.ICE_CREAM, quantity).getTotalPrice())
+                .isEqualTo(price);
+    }
 
     @Nested
     @DisplayName("생성 테스트")
