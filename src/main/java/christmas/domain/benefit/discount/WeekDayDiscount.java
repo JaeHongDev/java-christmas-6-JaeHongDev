5053869653f574b203cafcdd5422620afd28e084
JaehongDev <println@kakao.com>
2023-11-12 16:31:47 +0900

    feat: 평일 할인 기능 구현

---
+++ src/main/java/christmas/domain/benefit/discount/WeekDayDiscount.java
@@ -0,0 +1,18 @@
+package christmas.domain.benefit.discount;
+
+import christmas.domain.vo.Menu;
+import christmas.domain.vo.OrderItem;
+import christmas.domain.vo.OrderLine;
+import christmas.domain.vo.Payment;
+import java.time.LocalDate;
+
+public class WeekDayDiscount implements Discount {
+    @Override
+    public Payment apply(LocalDate date, OrderLine orderLine) {
+        return new Payment(orderLine.orderItems()
+                .stream()
+                .filter(orderItem -> Menu.DESSERT.contains(orderItem.food()))
+                .mapToInt(OrderItem::quantity)
+                .sum() * 2023);
+    }
+}
