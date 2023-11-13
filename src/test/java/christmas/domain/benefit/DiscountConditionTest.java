ba45ba628c81aab8c78815031b27fd9432d14869
JaehongDev <println@kakao.com>
2023-11-14 02:29:14 +0900

    test: 2023년 12월 일자 생성 코드 테스트코드에 적용

--- src/test/java/christmas/domain/benefit/DiscountConditionTest.java
+++ src/test/java/christmas/domain/benefit/DiscountConditionTest.java
@@ -1,8 +1,13 @@
 package christmas.domain.benefit;
 
+import static christmas.domain.benefit.DiscountCondition.CHRISTMAS_DISCOUNT;
+import static christmas.domain.benefit.DiscountCondition.SPECIAL_DISCOUNT;
+import static christmas.domain.benefit.DiscountCondition.WEEKDAY_DISCOUNT;
+import static christmas.domain.benefit.DiscountCondition.WEEKEND_DISCOUNT;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import christmas.domain.util.ChristmasLocalDate;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.IntStream;
@@ -19,17 +24,17 @@ class DiscountConditionTest {
     @ParameterizedTest
     @ValueSource(ints = {26, 27, 28, 29, 30, 31})
     void 크리스마스_할인_조건은_크리스마스_이후에는_유효하지_않습니다(int input) {
-        assertThat(DiscountCondition.CHRISTMAS_DISCOUNT.isSatisfy(LocalDate.of(2023, 12, input)))
-                .isFalse();
+        final var date = ChristmasLocalDate.create(input);
+
+        assertThat(CHRISTMAS_DISCOUNT.isSatisfy(date)).isFalse();
     }
 
 
     @Test
     void 크리스마스_할인_조건은_크리스마스_이전에만_적용됩니다() {
         IntStream.rangeClosed(1, 25)
-                .mapToObj(day -> LocalDate.of(2023, 12, day))
-                .forEach((date) -> assertThat(DiscountCondition.CHRISTMAS_DISCOUNT.isSatisfy(date))
-                        .isTrue());
+                .mapToObj(ChristmasLocalDate::create)
+                .forEach((date) -> assertThat(CHRISTMAS_DISCOUNT.isSatisfy(date)).isTrue());
     }
 
     @Test
@@ -43,10 +48,10 @@ class DiscountConditionTest {
         assertAll(
                 () -> weekDay.stream()
                         .map(day -> LocalDate.of(2023, 12, day))
-                        .forEach((date) -> assertThat(DiscountCondition.WEEKDAY_DISCOUNT.isSatisfy(date)).isTrue()),
+                        .forEach((date) -> assertThat(WEEKDAY_DISCOUNT.isSatisfy(date)).isTrue()),
                 () -> weekend.stream()
                         .map(day -> LocalDate.of(2023, 12, day))
-                        .forEach((date) -> assertThat(DiscountCondition.WEEKDAY_DISCOUNT.isSatisfy(date)).isFalse()));
+                        .forEach((date) -> assertThat(WEEKDAY_DISCOUNT.isSatisfy(date)).isFalse()));
     }
 
     @Test
@@ -59,17 +64,17 @@ class DiscountConditionTest {
 
         assertAll(
                 () -> weekDay.stream()
-                        .map(day -> LocalDate.of(2023, 12, day))
-                        .forEach((date) -> assertThat(DiscountCondition.WEEKEND_DISCOUNT.isSatisfy(date)).isFalse()),
+                        .map(ChristmasLocalDate::create)
+                        .forEach((date) -> assertThat(WEEKEND_DISCOUNT.isSatisfy(date)).isFalse()),
                 () -> weekend.stream()
-                        .map(day -> LocalDate.of(2023, 12, day))
-                        .forEach((date) -> assertThat(DiscountCondition.WEEKEND_DISCOUNT.isSatisfy(date)).isTrue()));
+                        .map(ChristmasLocalDate::create)
+                        .forEach((date) -> assertThat(WEEKEND_DISCOUNT.isSatisfy(date)).isTrue()));
     }
 
     @Test
-    void 특별할인은_특별한일_날에만_적용이_됩니다() {
+    void 특별할인은_특별한날에만_적용이_됩니다() {
 
-        final var specialDay = List.of(3, 10, 17, 24, 31);
+        final var specialDay = List.of(3, 10, 17, 24, 25, 31);
         final var notSpecialDay = IntStream.rangeClosed(1, 31)
                 .filter(day -> !specialDay.contains(day))
                 .boxed()
@@ -77,10 +82,10 @@ class DiscountConditionTest {
         assertAll(
                 () -> specialDay.stream()
                         .map(day -> LocalDate.of(2023, 12, day))
-                        .forEach((date) -> assertThat(DiscountCondition.SPECIAL_DISCOUNT.isSatisfy(date)).isTrue()),
+                        .forEach((date) -> assertThat(SPECIAL_DISCOUNT.isSatisfy(date)).isTrue()),
                 () -> notSpecialDay.stream()
                         .map(day -> LocalDate.of(2023, 12, day))
-                        .forEach((date) -> assertThat(DiscountCondition.SPECIAL_DISCOUNT.isSatisfy(date)).isFalse())
+                        .forEach((date) -> assertThat(SPECIAL_DISCOUNT.isSatisfy(date)).isFalse())
         );
     }
 }
