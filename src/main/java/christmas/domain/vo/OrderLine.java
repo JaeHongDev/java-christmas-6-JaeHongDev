2aa72451d6459b1bd786e3ef93240d4a50d4a2c6
JaehongDev <println@kakao.com>
2023-11-14 02:18:44 +0900

    refactor: 주문 항목 조건식 간소화

--- src/main/java/christmas/domain/vo/OrderLine.java
+++ src/main/java/christmas/domain/vo/OrderLine.java
@@ -9,12 +9,26 @@ import java.util.Objects;
 public record OrderLine(
         List<OrderItem> orderItems
 ) {
+    private static final int MAX_ORDER_QUANTITY = 20;
 
     public OrderLine {
         INVALID_ORDER.invokeByCondition(Objects.isNull(orderItems));
-        INVALID_ORDER.invokeByCondition(orderItems.isEmpty());
-        INVALID_ORDER.invokeByCondition(orderItems.stream().mapToInt(OrderItem::quantity).sum() > 20);
-        INVALID_ORDER.invokeByCondition(orderItems.stream().map(OrderItem::food).allMatch(Menu.BEVERAGE::contains));
+        INVALID_ORDER.invokeByCondition(orderItems::isEmpty);
+        INVALID_ORDER.invokeByCondition(isOrderQuantityExceeded(orderItems));
+        INVALID_ORDER.invokeByCondition(hasOnlyBeverage(orderItems));
     }
-    
+
+    // compact 생성자가 동작을 완료하기 전 까지 메서드에서 orderItems를 참조할 수 없다.
+
+    private static boolean isOrderQuantityExceeded(List<OrderItem> orderItems) {
+        return orderItems.stream()
+                .mapToInt(OrderItem::quantity)
+                .sum() > MAX_ORDER_QUANTITY;
+    }
+
+    public static boolean hasOnlyBeverage(List<OrderItem> orderItems) {
+        return orderItems.stream()
+                .map(OrderItem::food)
+                .allMatch(Menu.BEVERAGE::contains);
+    }
 }
