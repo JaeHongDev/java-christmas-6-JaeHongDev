c517a959548829096482d7fca27e5fe513c707f1
JaehongDev <println@kakao.com>
2023-11-14 14:22:46 +0900

    style: 주문 테스트 케이스 추가

--- src/test/java/christmas/domain/entity/OrderTest.java
+++ src/test/java/christmas/domain/entity/OrderTest.java
@@ -3,6 +3,7 @@ package christmas.domain.entity;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import christmas.domain.util.ChristmasLocalDate;
 import christmas.domain.vo.Food;
 import christmas.domain.vo.OrderItem;
 import christmas.domain.vo.OrderLine;
@@ -13,6 +14,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -43,6 +46,36 @@ class OrderTest {
                 () -> assertThat(menus.get(Food.BBQ_RIBS.getName())).isEqualTo(10),
                 () -> assertThat(menus.get(Food.RED_WINE.getName())).isEqualTo(5)
         );
+    }
+
+    @Test
+    void _1만원_보다_작은_경우는_할인_적용이_불가능합니다() {
+        final var order = new Order(ChristmasLocalDate.create(25), new OrderLine(
+                List.of(
+                        new OrderItem(Food.ICE_CREAM, 1)
+                )
+        ));
+        assertAll(
+                () -> assertThat(order.applyDiscount().totalPrice()).isEqualTo(new Payment(5000)),
+                () -> assertThat(order.applyDiscount().calculateAmountAfterDiscount()).isEqualTo(5000)
+        );
+    }
+
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+            2,10000,5954,
+            3,15000,8931
+            """)
+    void _1만원_이상의_경우_할인이_적용됩니다(int quantity, int payment, int discountAmount) {
+        final var order = new Order(ChristmasLocalDate.create(28), new OrderLine(
+                List.of(new OrderItem(Food.ICE_CREAM, quantity))
+        ));
+        final var benefitDetails = order.applyDiscount();
+        assertAll(
+                () -> assertThat(benefitDetails.totalPrice()).isEqualTo(new Payment(payment)),
+                () -> assertThat(benefitDetails.calculateAmountAfterDiscount()).isEqualTo(discountAmount)
+        );
+
     }
 
 
